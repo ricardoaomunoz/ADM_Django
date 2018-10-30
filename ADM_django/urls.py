9fe852469fc72aa89de2e595eb3b59368cf9cf9c
Ricardo Osorio <richard10_04@hotmail.com>
2018-10-29 23:59:10 -0500

    Django proyect

---
+++ ADM_django/urls.py
@@ -0,0 +1,16 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+from rest_framework import routers
+from ADM_django.Login import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
